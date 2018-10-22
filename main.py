a2a561570119cd53c400276f7f967b560a3737af
Gail Fike <gfike19@gmail.com>
2018-10-21 23:07:01 -0500

    found how to convert hex to int

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, redirect, render_template
 import os
-import jinja2
+# import jinja2
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+# template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+# jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -14,16 +14,16 @@ def indexGet():
 
 @app.route("/", methods=["POST"])
 def indexPost():
-    favcolor = request.form['favcolor']
+    # below removes # at start and converts to decimal
+    # favcolor = str(request.form['favcolor'])[1:]
+    # conv = int(str(request.form['favcolor'])[1:], 16)
+
+    favcolor = str(request.form['favcolor'])
+    # color input is unicode type from request.form
     #/pagename?(evaluate)variableWhichHas=value
     #method=post is needed on form
     return render_template("lastcolor.html", favcolor=favcolor)
     
-# @app.route("/lastcolor", methods=['GET'])
-# def lastColorGet():
-#     test = "words"
-#     favcolor = request.args.get("favcolor")
-#     return render_template("lastcolor.html", favcolor=favcolor, test=test)
 
 if __name__ == "__main__":
     app.run()
